08eff7d01e2a03ff582efa687a8b6bb8587283e1
YvannEmate1 <134917542+YvannEmate1@users.noreply.github.com>
2026-04-03 00:07:59 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Topbar from '../components/Topbar';
 import Navbar from '../components/NavbarClean';
 import HeroMain from '../components/HeroMain';
 import OurBusinesses from '../components/OurBusinesses';
+import FamActivities from '../components/FamActivities';
 import Partners from '../components/Partners';
 import Footer from '../components/Footer';
 
@@ -12,6 +13,7 @@ export default function HomePage() {
       <Topbar />
       <Navbar />
       <HeroMain />
+      <FamActivities />
       <OurBusinesses />
       <Partners />
       <Footer />
